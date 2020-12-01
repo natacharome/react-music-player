67674e30c96fe8e27e2c480d4ce36b24873dd9eb
Natacha Rome <natacha-rome@hotmail.fr>
2020-12-01 12:06:26 +0100

    First commit

---
+++ react-player/src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import "./styles/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Music Player</h1>
+    </div>
+  );
+}
+
+export default App;
