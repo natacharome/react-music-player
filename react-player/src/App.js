d62463753f8c7ac300008089ec0a9c89300c481b
Natacha Rome <natacha-rome@hotmail.fr>
2020-12-01 15:17:19 +0100

    Implement player

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, {useState} from "react";
+//Import styles 
+
 import "./styles/app.scss";
 
+// Util
+import data from "./data";
+
+//Adding components
+import Player from './components/Player';
+import Song from './components/Song';
+
 function App() {
+
+  // State 
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song currentSong={currentSong} />
+      <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>
     </div>
   );
 }
